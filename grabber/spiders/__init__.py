86a94664de47e7fca8e0dd5f4b96020ce59bd41b
niko <niko@iMac3.(none)>
2012-05-10 18:58:38 +0300

    Crawl site and save pages

--- grabber/spiders/__init__.py
+++ grabber/spiders/__init__.py
@@ -4,48 +4,87 @@
 # your spiders.
 
 import sys
-import transaction
+import time
 
-from scrapy.spider import BaseSpider
+from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy import log
 from scrapy.http import Request
 
 from sitegrabber.models import DBSession, Base, WebPage, WebSite
 from grabber.settings import WEB_APP_SETTINGS
 from sqlalchemy import engine_from_config
+from sqlalchemy.orm import Session
 
-class GrabberSpider(BaseSpider):
+class GrabberSpider(CrawlSpider):
     name = "grabber"
-    allowed_domains = ["*"]
+    allowed_domains = ["www.lierd.com"]
+    
+    SCRAPED_URLS = []
     
     # Let's think how to pass here url
     start_urls = [
         'http://www.lierd.com/english/'
     ]
     
+    rules = [
+        Rule(SgmlLinkExtractor(), callback='parse_item'),
+    ]
+    
     def __init__(self, *args, **kw):
+        SCRAPED_DOMAIN = "www.lierd.com"
         super(GrabberSpider, self).__init__(*args, **kw)
         log.msg('Init SQL alchemy engine', level=log.DEBUG)
         engine = engine_from_config(WEB_APP_SETTINGS, 'sqlalchemy.')
-        DBSession.configure(bind=engine)
+        conn = engine.connect()
+        self.dbsession = Session(bind=conn)
+        
+        # patch orm objects to use this local session object
+        WebPage.session = self.dbsession
+        
         Base.metadata.create_all(engine) # while use creating DB here
         
-        q = DBSession.query(WebSite).filter(WebSite.original_url == "www.lierd.com")
+        q = self.dbsession.query(WebSite).filter(WebSite.original_url == SCRAPED_DOMAIN)
         website = q.first()
         if website is None:
-            website = WebSite(original_url="www.lierd.com", local_domain='test.localhost')
-            DBSession.add(website)
-            DBSession.flush()
-            transaction.commit()
+            website = WebSite(original_url=SCRAPED_DOMAIN, local_domain='test.localhost')
+            self.dbsession.add(website)
+            self.dbsession.commit()
         self.website = website
         
-    def parse(self, response):
-        
-        self.handle_page(response)
     
-        hxs = HtmlXPathSelector(response)
+    def prepare_link(self, url, current_url):
+        '''
+            Make proper uri from given url
+        '''
+        if not current_url.endswith('/'): current_url += '/'
+        
+        # ignore javascript links
+        for s in ['javascript:', 'mailto:', '#']:
+            if url.startswith(s): return None
+        
+        if url.find('http://') != -1 or url.find('https://') != -1:
+            # in case we have complete http or https protocol uri
+            url = url.replace('http://', '').replace('https://', '')
+            url_domain = url.split('/')[0]
+            if url_domain == self.website.original_url: return url # Scrape just this site urls
+            else: return None # don't scrape external links
+        
+        if url.startswith('/'):
+            # we get absolute url
+            return "http://%s%s" % (self.website.original_url, url)
+        else:
+            return "%s%s" % (current_url, url)
             
+        
+    def parse_item(self, response):
+        
+        log.msg('I\'m here: %s' % response.url, level=log.DEBUG)
+        if 'text/html' in response.headers['Content-Type']:
+            self.handle_page(response)
+        hxs = HtmlXPathSelector(response)
+        '''
         for css in hxs.select('//link/@href').extract():
             log.msg(css, level=log.DEBUG)
             
@@ -54,9 +93,19 @@ class GrabberSpider(BaseSpider):
             
         for js in hxs.select('//script/@src').extract():
             log.msg(js, level=log.DEBUG)
-
+        
         for url in hxs.select('//a/@href').extract():
+            log.msg('Going by link %s' % url, level=log.DEBUG)
+            if WebPage.exists(self._get_path(url)):
+                continue
+            url = self.prepare_link(url, response.url)
+            if not url:
+                continue
+            if url in self.__class__.SCRAPED_URLS: continue
+            self.__class__.SCRAPED_URLS.append(url)
+            time.sleep(0.1)
             yield Request(url, callback=self.parse)
+        '''
 
     
     def _get_path(self, url):
@@ -68,7 +117,6 @@ class GrabberSpider(BaseSpider):
     def handle_page(self, response):
         path = self._get_path(response.url)
         log.msg('Scraping page %s' % path, level=log.DEBUG)
-        page = WebPage(uri=path, content=response.body, website=self.website)
-        DBSession.add(page)
-        DBSession.flush()
-        transaction.commit()
+        content = response.body.decode(response.encoding)
+        WebPage.add(uri=path, content=content, website=self.website)
+        self.dbsession.commit()
--- sitegrabber/models.py
+++ sitegrabber/models.py
@@ -24,6 +24,8 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 
 class Base(object):
 
+    session = DBSession # static propery may be overrided for some purposes
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
@@ -36,13 +38,32 @@ class Base(object):
 Base = declarative_base(cls=Base)
 
 class WebPage(Base):
-    uri = Column(String(256), nullable=False)
+    uri = Column(String(256), nullable=False, unique=True)
     content = Column(Text) # Site body and content
     website_id = Column(Integer(), ForeignKey('website.id'))
     website = relation('WebSite', backref='pages')
     
+    @classmethod
+    def add(cls, **kw):
+        uri = kw.pop('uri', None)
+        if uri is None:
+            raise Exception('uri parameter is required for this function')
+        # try to find page with this uri first
+        page = cls.session.query(cls).filter(cls.uri == uri).first()
+        if page:
+            page.__dict__.update(kw)
+        else:
+            page = cls(uri=uri, **kw)
+        cls.session.add(page)
+        cls.session.flush()
+    
+    @classmethod
+    def exists(cls, url):
+        page = cls.session.query(cls).filter(cls.uri == url).first()
+        return page
+
 class WebSite(Base):
-    original_url = Column(String(256), nullable=False)
+    original_url = Column(String(256), nullable=False, unique=True)
     local_domain = Column(String(256), nullable=False)
     descr = Column(Text) # Site description
     status = Column(Integer(), default=0) # Status of site indexing
